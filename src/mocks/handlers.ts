39fbca29e8221ff5392ab1bc5799feb8922014de
tommya98 <tommya98@naver.com>
2024-02-13 15:58:44 +0900

    FEAT: msw 핸들러 완성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,35 @@
 import { http, delay, HttpResponse } from "msw";
 
 const handlers = [
-  http.get("/products", async () => {
+  http.get("/products", async ({ request }) => {
     await delay(500);
-    return HttpResponse.json({ products }, { status: 200 });
+
+    const url = new URL(request.url);
+    const page = Number(url.searchParams.get("page"));
+    const size = Number(url.searchParams.get("size"));
+    const start = (page - 1) * size;
+    const end = start + size;
+
+    const paginatedItems = products.slice(start, end);
+
+    const responseContent = {
+      page,
+      size,
+      total: products.length,
+      totalPages: Math.ceil(products.length / size),
+      products: paginatedItems,
+    };
+
+    return HttpResponse.json({ responseContent }, { status: 200 });
   }),
 ];
 
 export default handlers;
 
-const products = [
-  {
-    name: "Product 1",
-    price: 100,
-  },
-  {
-    name: "Product 2",
-    price: 200,
-  },
-  {
-    name: "Product 3",
-    price: 150,
-  },
-];
+const products = Array.from(Array(1000).keys()).map((id) => {
+  return {
+    name: `상품${id}`,
+    price: 10000 + id * 100,
+    imageUrl: `https://picsum.photos/id/${id}/200/300`,
+  };
+});
