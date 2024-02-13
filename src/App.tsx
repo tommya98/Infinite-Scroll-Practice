6b6a212b5d7fd4fce92ad1fd059c6a41845d7bc0
tommya98 <tommya98@naver.com>
2024-02-13 16:39:18 +0900

    FEAT: 기본 무한 스크롤 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,65 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import Axios from "axios";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
 import Item from "./Item";
 
+interface ItemProps {
+  name: string;
+  price: number;
+  imageUrl: string;
+}
+
+interface IResponseContent {
+  responseContent: {
+    page: number;
+    size: number;
+    total: number;
+    totalPages: number;
+    products: ItemProps[];
+  };
+}
+
+const PAGE_SIZE = 40;
+
 function App() {
-  const [items, setItems] = useState([
-    {
-      name: "상품1",
-      price: 10000,
-      imageUrl: "https://picsum.photos/id/1/200/300",
-    },
-    {
-      name: "상품2",
-      price: 20000,
-      imageUrl: "https://picsum.photos/id/2/200/300",
-    },
-    {
-      name: "상품3",
-      price: 30000,
-      imageUrl: "https://picsum.photos/id/3/200/300",
-    },
-    {
-      name: "상품4",
-      price: 40000,
-      imageUrl: "https://picsum.photos/id/4/200/300",
-    },
-  ]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [items, setItems] = useState<ItemProps[]>([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [isFetching, setFetching] = useState(false);
+
+  const fetchItems = useCallback(async () => {
+    const response = await Axios.get<IResponseContent>(
+      `/products?page=${pageNumber}&size=${PAGE_SIZE}`
+    );
+    const { products, page, totalPages } = response.data.responseContent;
+
+    setItems((prevItems) => [...prevItems, ...products]);
+    setPageNumber(page + 1);
+    setHasNextPage(page < totalPages);
+    setFetching(false);
+  }, [pageNumber]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      // console.log(scrollTop, offsetHeight, window.innerHeight);
+      if (
+        window.innerHeight + scrollTop >=
+        offsetHeight - window.innerHeight * 2
+      ) {
+        setFetching(true);
+      }
+    };
+    setFetching(true);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (isFetching && hasNextPage) fetchItems();
+    else if (!hasNextPage) setFetching(false);
+  }, [isFetching]);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
